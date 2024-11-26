9aada76613b735c288c17df02e2e74bf625d7ec8
tawseelgroup <tawseelgroup943@gmail.com>
2024-11-26 16:37:24 +0300

    form template alignment

--- main/forms.py
+++ main/forms.py
@@ -1,11 +1,12 @@
+from django.forms import inlineformset_factory, ModelForm
 from django import forms
 from .models import Item, Group, Store
 
 
-class ItemForm(forms.ModelForm):
+class ItemForm(ModelForm):
     class Meta:
-        model=Item
-        fields= ['itemName', 'itemDescription', 'store', 'group', 'count', 'actualCount']
+        model=Item 
+        fields= ['itemName', 'itemDescription',  'store', 'group', 'count', 'actualCount']
     
 
     itemName = forms.CharField(widget=forms.TextInput(attrs={'class': 'inputs', 'placeholder': 'Enter Item Names'}))
@@ -16,4 +17,17 @@ class ItemForm(forms.ModelForm):
     actualCount = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'inputs', 'placeholder': 'Enter Item Price'}))
 
 
+
+
+
+# formset = inlineformset_factory(Group, Item,  extra=1)
+
+
+    # widgets = {
+    #     'itemName' : forms.TextInput(attrs={'class': 'inputs', 'placeholder': 'Enter Item Names'}),
+    #     'itemDescription' : forms.Textarea(attrs={'class': 'inputs', 'placeholder': 'Enter Item Names'}),
+    #     # 'store' : forms.ModelChoiceField(forms.Select(attrs={'class': 'inputs'}), queryset = Store.objects.all()),
+    #     # 'group' : forms.ModelChoiceField(queryset=Group.objects.all(),  required=True, attrs={'class': 'inputs', 'placeholder': 'Enter Item Price'}),
+    #     'actualCount' : forms.NumberInput(attrs={'class': 'inputs', 'placeholder': 'Enter Item Price'}),
+    # }
 
